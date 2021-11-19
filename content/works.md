8829ea12a264c62f1b0250f92d9b55efacbd1c0a
qinyuanpei <yuanpei@wangdou-hbean.com>
2021-11-19 08:49:27 +0800

    首页增加项目模块

---
+++ content/works.md
@@ -0,0 +1,5 @@
+---
+title: Works
+subtitle: Posts, tutorials, snippets, musings, and everything else.
+layout: "works"
+---
--- themes/hugo-tania/assets/ts/reward.ts
+++ themes/hugo-tania/assets/ts/reward.ts
@@ -4,38 +4,46 @@ const closeButton = document.querySelector('.cy-close-btn');
 const chioceItems = document.querySelectorAll('.platform-item');
 
 export let initialReward = function() {
-    rewardButton.addEventListener('click', () => {
-        rewardBox.style.display ='block';
-    });
+    if (rewardButton != null) {
+        rewardButton.addEventListener('click', () => {
+            rewardBox.style.display ='block';
+        });
+    }
 
-    closeButton.addEventListener('click', () => {
-        rewardBox.style.display ='none';
-    });
+    if (closeButton != null) {
+        closeButton.addEventListener('click', () => {
+            rewardBox.style.display ='none';
+        });
+    }
 
-    chioceItems.forEach(item => {
-        item.addEventListener('click', ()=> {
-            let radios = document.querySelectorAll('.platform-radio-box');
-            radios.forEach(radio => {
-                radio.classList.remove('platform-checked');
-                radio.style.background = 'url(/imgs/reward/radio2.jpg)';
-            });
-
-            let radio = item.querySelector('.platform-radio-box');
-            radio.classList.add('platform-checked');
-            radio.style.background = 'url(/imgs/reward/radio1.jpg)';
-            let choiceId = radio.attributes['data-id'].value;
-
-            let qrcodes = document.querySelectorAll('.cy-reward-code img');
-            qrcodes.forEach(qrcode => {
-                let nodeType = choiceId + '-code';
-                if (qrcode.attributes['node-type'].value == nodeType){
-                    qrcode.style.display ='inline';
-                } else {
-                    qrcode.style.display ='none';
-                }
-            });
-        })
-    });
+    if (chioceItems != null) {
+        chioceItems.forEach(item => {
+            item.addEventListener('click', ()=> {
+                let radios = document.querySelectorAll('.platform-radio-box');
+                radios.forEach(radio => {
+                    radio.classList.remove('platform-checked');
+                    radio.style.background = 'url(/imgs/reward/radio2.jpg)';
+                });
     
-    chioceItems[1].click();
+                let radio = item.querySelector('.platform-radio-box');
+                radio.classList.add('platform-checked');
+                radio.style.background = 'url(/imgs/reward/radio1.jpg)';
+                let choiceId = radio.attributes['data-id'].value;
+    
+                let qrcodes = document.querySelectorAll('.cy-reward-code img');
+                qrcodes.forEach(qrcode => {
+                    let nodeType = choiceId + '-code';
+                    if (qrcode.attributes['node-type'].value == nodeType){
+                        qrcode.style.display ='inline';
+                    } else {
+                        qrcode.style.display ='none';
+                    }
+                });
+            })
+        });
+        
+        if (chioceItems[1] != null) {
+            chioceItems[1].click();
+        }
+    }
 }

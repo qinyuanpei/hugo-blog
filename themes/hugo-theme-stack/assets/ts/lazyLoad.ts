99928e0865d8a6c34933176ee50000ea29c2e702
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-07-26 09:39:07 +0800

    继续测试

--- themes/hugo-theme-stack/assets/ts/lazyLoad.ts
+++ themes/hugo-theme-stack/assets/ts/lazyLoad.ts
@@ -12,7 +12,8 @@ let lazyLoad = function(imgs) {
     for (var i = 0; i < imgs.length; i++) {
         if (H + S > getTop(imgs[i])) {
             if (imgs[i].getAttribute('loading') == 'lazy' && imgs[i].getAttribute('data-src')) {
-                let src = decodeURIComponent(imgs[i].getAttribute('data-src'))
+                let src = decodeURI(imgs[i].getAttribute('data-src'))
+                console.log(src)
                 imgs[i].src = src
             }
         }

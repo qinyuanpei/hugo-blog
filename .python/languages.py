633c04e3ec6a40ff6c59c2d58b1c4c3e1ff858e9
qinyuanpei <yuanpei@wangdou-hbean.com>
2022-08-05 13:56:26 +0800

    使用兼容写法

--- .python/languages.py
+++ .python/languages.py
@@ -1,6 +1,4 @@
 ﻿import os
-import sys
-import re
 import json
 
 # 列举所有博客
@@ -42,13 +40,13 @@ def analyseLanguages(posts):
                     if language.lower() == "yaml" or language.lower() == "yml":
                         language = language.upper()
                     if language in ['shell','json','csharp','lua','yaml','yml','plain']:
-                        print('{post} maybe need a check for code blocks.'.format(post="post"))
+                        print('{post} maybe need a check for code blocks.'.format(post=post))
                     if language in languages.keys():
                         languages[language] = languages[language] + 1
                     else:
                         languages[language] = 1
         except Exception as ex:
-            print(f'{post} maybe need a check for encoding' )
+            print('{post} maybe need a check for encoding.'.format(post=post))
 
     #排序后取前5种语言输出        
     languages = sorted(languages.items(), key=lambda d:d[1], reverse = True)
